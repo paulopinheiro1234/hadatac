2980fb8bf70d6c85f7a7d43a5337ce148b917995
Henrique O. Santos <hensantos@yahoo.com>
2015-08-14 11:00:55 -0400

    Created initialization scripts for unmaneged solr services.

---
+++ hadatac.solr/solr4.in.sh
@@ -0,0 +1,5 @@
+SOLR_PID_DIR=~/solr-4.10.4
+SOLR_HOME=~/hadatac/hadatac.solr/solrdf-home
+LOG4J_PROPS=~/solr-4.10.4/example/resources/log4j.properties
+SOLR_LOGS_DIR=~/solr-4.10.4/example/logs
+SOLR_PORT=7574
---
+++ hadatac.solr/solr5.in.sh
@@ -0,0 +1,5 @@
+SOLR_PID_DIR=~/solr-5.2.1
+SOLR_HOME=~/hadatac/hadatac.solr/solr-home
+LOG4J_PROPS=~/solr-5.2.1/server/resources/log4j.properties
+SOLR_LOGS_DIR=~/solr-5.2.1/server/logs
+SOLR_PORT=8983
