bd8b6cadd7408ad7d6e1a308c13a8c7d4a132325
paulopinheiro1234 <pp3223@gmail.com>
2015-10-05 12:37:52 -0400

    start up solr repositories at play start up, if solr repositories are down

--- app/org/hadatac/utils/Repository.java
+++ app/org/hadatac/utils/Repository.java
@@ -4,10 +4,18 @@ import org.hadatac.data.loader.DataContext;
 import org.hadatac.metadata.loader.MetadataContext;
 import org.hadatac.metadata.loader.PermissionsContext;
 
+import play.Play;
+
 public class Repository {
 
 	public static final String METADATA = "metadata";
 	public static final String DATA     = "data";	
+	
+	public static final String START = "start";
+	public static final String STOP  = "stop";
+	
+	private static final String METADATA_SCRIPT = "run_solr4.sh";
+	private static final String DATA_SCRIPT     = "run_solr5.sh";
 	
     public static boolean operational(String repository) {
     	if (repository.equals(METADATA)) {
@@ -21,5 +29,30 @@ public class Repository {
     	return false;
     }
 
+    public static String startStopMetadataRepository(String oper, String repository) {
+    	String message = "";
+    	String script = "";
+        if (!oper.equals(START) && !oper.equals(STOP)) {
+        	message = Feedback.println(Feedback.WEB, "Invalid operation. It should be either " + START + " or " + STOP);
+        	return message;
+        }
+    	String home = Play.application().configuration().getString("hadatac.solr.home");
+    	if (!home.endsWith("/")) {
+    		home = home + "/";
+    	}
+    	if (repository.equals(METADATA)) {
+    		script = home + METADATA_SCRIPT;
+    	} else if (repository.equals(DATA)) {
+    	    script = home + DATA_SCRIPT;	
+    	} else {
+    		message = "FAIL";
+    		return message;
+    	}
+	    String[] cmd = {script, oper};
+		message += Feedback.print(Feedback.WEB, "Requested " + oper + " " + repository + " repository.");                
+	    message += Command.exec(Feedback.WEB, false, cmd);
+    	return message;
+    }
+	
 }	
 	
