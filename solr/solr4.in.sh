a8aa82048d32cef6d9ffebc4094b3b6d17a3bf91
Sabbir <rashidsabbir@gmail.com>
2016-08-09 14:58:21 -0400

    Changed hadatac.conf to work with my solr install directory. The original line is commented out. Make sure to change this when installing

--- solr/solr4.in.sh
+++
@@ -1,5 +0,0 @@
-SOLR_PID_DIR=$HADATAC_SOLR/solr-4.10.4
-SOLR_HOME=$HADATAC_SOLR/solrdf-home
-LOG4J_PROPS=$HADATAC_SOLR/solr-4.10.4/example/resources/log4j.properties
-SOLR_LOGS_DIR=$HADATAC_SOLR/solr-4.10.4/example/logs
-SOLR_PORT=7574
--- solr/solr5.in.bat
+++
@@ -1,5 +0,0 @@
-set SOLR_PID_DIR=%HADATAC_SOLR%\solr-5.2.1
-set SOLR_HOME=%HADATAC_SOLR%\solr-home
-set LOG4J_PROPS=%HADATAC_SOLR%\solr-5.2.1\server\resources\log4j.properties
-set SOLR_LOGS_DIR=%HADATAC_SOLR%\solr-5.2.1\server\logs
-set SOLR_PORT=8983
