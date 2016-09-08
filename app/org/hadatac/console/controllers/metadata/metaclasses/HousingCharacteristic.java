2884f969368cf6fc14d9f9cdcbf387f6db4636f3
Sabbir <rashidsabbir@gmail.com>
2016-09-08 19:30:15 -0400

    New files for metaclass hierarchy browsers

---
+++ app/org/hadatac/console/controllers/metadata/metaclasses/HousingCharacteristic.java
@@ -0,0 +1,70 @@
+package org.hadatac.console.controllers.metadata.metaclasses;
+
+import org.hadatac.console.http.GetSparqlQuery;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.TreeMap;
+
+import org.hadatac.console.models.SparqlQuery;
+import org.hadatac.console.models.OtMSparqlQueryResults;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import org.hadatac.console.views.html.metadata.metaclasses.metaclasses_browser;
+import org.hadatac.console.views.html.error_page;
+
+
+public class HousingCharacteristic extends Controller {
+	
+	// for /metadata HTTP GET requests
+    public static Result index() {
+
+    	SparqlQuery query = new SparqlQuery();
+        GetSparqlQuery query_submit = new GetSparqlQuery(query);
+        OtMSparqlQueryResults theResults;
+        String tabName = "HousingCharacteristic";
+        String query_json = null;
+        System.out.println("HousingCharacteristic.java is requesting: " + tabName);
+        try {
+            query_json = query_submit.executeQuery(tabName);
+            //System.out.println("query_json = " + query_json);
+            theResults = new OtMSparqlQueryResults(query_json, true);
+        } catch (IllegalStateException | IOException | NullPointerException e1) {
+            return internalServerError(error_page.render(e1.toString(), tabName));
+            //e1.printStackTrace();
+        }
+        System.out.println("HousingCharacteristic index() was called!");
+    	
+    	//List<org.hadatac.entity.pojo.Entity> entities = org.hadatac.entity.pojo.Entity.find();
+    	
+        return ok(metaclasses_browser.render(theResults, "HousingCharacteristic"));
+    }// /index()
+
+
+    // for /metadata HTTP POST requests
+    public static Result postIndex() {
+
+    	SparqlQuery query = new SparqlQuery();
+        GetSparqlQuery query_submit = new GetSparqlQuery(query);
+        OtMSparqlQueryResults theResults;
+        String tabName = "HousingCharacteristic";
+        String query_json = null;
+        System.out.println("HousingCharacteristic.java is requesting: " + tabName);
+        try {
+            query_json = query_submit.executeQuery(tabName);
+            //System.out.println("query_json = " + query_json);
+            theResults = new OtMSparqlQueryResults(query_json, true);
+        } catch (IllegalStateException | IOException | NullPointerException e1) {
+            return internalServerError(error_page.render(e1.toString(), tabName));
+            //e1.printStackTrace();
+        }
+        System.out.println("HousingCharacteristic index() was called!");
+    	
+    	//List<org.hadatac.entity.pojo.Entity> entities = org.hadatac.entity.pojo.Entity.find();
+    	
+        return ok(metaclasses_browser.render(theResults, "HousingCharacteristic"));
+    }// /postIndex()
+
+}
