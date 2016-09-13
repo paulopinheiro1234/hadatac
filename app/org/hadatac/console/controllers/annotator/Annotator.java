f9a9d446719af45c86e27d7f3ad3747182f712ff
gychant <gychant@qq.com>
2016-09-13 18:18:40 -0400

    Added auto ccsv generation functionality

--- app/org/hadatac/console/controllers/annotator/Annotator.java
+++ app/org/hadatac/console/controllers/annotator/Annotator.java
@@ -5,6 +5,7 @@ import org.apache.commons.io.IOUtils;
 import org.hadatac.console.controllers.AuthApplication;
 import org.hadatac.console.controllers.triplestore.LoadKB;
 import org.hadatac.console.controllers.triplestore.UserManagement;
+import org.hadatac.console.http.DataAcquisitionSchemaQueries;
 import org.hadatac.console.http.DeploymentQueries;
 import org.hadatac.console.http.GetSparqlQuery;
 
@@ -22,6 +23,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.StringTokenizer;
 
 import org.hadatac.console.models.SparqlQuery;
 import org.hadatac.console.models.SparqlQueryResults;
@@ -40,6 +42,7 @@ import org.hadatac.console.views.html.error_page;
 import org.hadatac.console.views.html.annotator.*;
 import org.hadatac.data.api.DataFactory;
 import org.hadatac.entity.pojo.DataCollection;
+import org.hadatac.metadata.loader.ValueCellProcessing;
 import org.hadatac.utils.NameSpaces;
 import org.hadatac.utils.State;
 
@@ -217,134 +220,11 @@ public class Annotator extends Controller {
     		
     		//System.out.println("uploadCSV: dep_json is " + dep_json);
     		//System.out.println("uploadCSV: ec_json is " + ec_json);
-    	} else 
-    	{
+    	} else {
     		handler = new CSVAnnotationHandler(uri, "", "");
     	}
 
     	return ok(uploadCSV.render(handler, "init",""));
         
     }// /postIndex()
-	
-//	@Restrict(@Group(AuthApplication.DATA_OWNER_ROLE))
-//    public static Result toggleAutoAnnotator() {
-//		Properties prop = new Properties();
-//		try {
-//			InputStream is = LoadKB.class.getClassLoader().getResourceAsStream("autoccsv.config");
-//			prop.load(is);
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		String folder_path = prop.getProperty("path");
-//		File folder = new File(folder_path);
-//		File[] listOfFiles = folder.listFiles();
-//
-//		for (int i = 0; i < listOfFiles.length; i++) {
-//			if (listOfFiles[i].isFile()) {
-//				System.out.println("File " + listOfFiles[i].getName());
-//			} else if (listOfFiles[i].isDirectory()) {
-//				System.out.println("Directory " + listOfFiles[i].getName());
-//			}
-//		}
-//
-//		return "";
-//	}
-//	
-//	@Restrict(@Group(AuthApplication.DATA_OWNER_ROLE))
-//    public static Result annotateCSVFile(String file_name) {
-//		State state = new State(State.ALL);
-//    	final User user = AuthApplication.getLocalUser(Controller.session());
-//		String ownerUri = UserManagement.getUriByEmail(user.email);
-//    	List<DataCollection> theResults = DataCollection.find(ownerUri, state);
-//		File newFile = new File(file_name);
-//		
-//		
-//
-//		NameSpaces ns = NameSpaces.getInstance();
-//		String preamble = Downloads.FRAG_START_PREAMBLE;
-//		preamble += ns.printNameSpaceList();
-//		preamble += "\n";
-//
-//		//Insert KB    	
-//		preamble += Downloads.FRAG_KB_PART1;
-//		preamble += Play.application().configuration().getString("hadatac.console.kb"); 
-//		preamble += Downloads.FRAG_KB_PART2;
-//
-//		try {
-//			//Insert Data Set
-//			preamble += "<" + DataFactory.getNextURI(DataFactory.DATASET_ABBREV) + ">";
-//			preamble += Downloads.FRAG_DATASET;
-//			preamble += handler.getDataCollectionUri() + ">; ";
-//
-//			int i = 0;
-//			int timeStampIndex = -1;
-//			ArrayList<Integer> mt = new ArrayList<Integer>();
-//			for (String str : handler.getFields()) {
-//				System.out.println(str);
-//				System.out.println("get " + i + "-entity: [" + p.getProperty(i + "-entity") + "]");
-//				System.out.println("get " + i + "-characteristic: [" + p.getProperty(i + "-characteristic") + "]");
-//				System.out.println("get " + i + "-unit:           [" + p.getProperty(i + "-unit") + "]");
-//				if ((p.getProperty(i + "-entity") != null) && 
-//						(!p.getProperty(i + "-entity").equals("")) &&
-//						(p.getProperty(i + "-characteristic") != null) && 
-//						(!p.getProperty(i + "-characteristic").equals("")) && 
-//						(p.getProperty(i + "-unit") != null) && 
-//						(!p.getProperty(i + "-unit").equals(""))) {
-//
-//					if (p.getProperty(i + "-unit").equals(Downloads.FRAG_IN_DATE_TIME)) {
-//						timeStampIndex = i; 
-//					} else {
-//						mt.add(i);
-//					}
-//				}
-//				i++;
-//			}
-//
-//			preamble += Downloads.FRAG_HAS_MEASUREMENT_TYPE;	
-//			int aux = 0;
-//			for (Integer mt_count : mt) {
-//				preamble += Downloads.FRAG_MT + aux + "> ";
-//				if(aux != (mt.size() - 1)){
-//					preamble += ", ";
-//				}
-//				aux++;
-//			}
-//			preamble += ".\n\n";
-//			System.out.println(preamble);
-//
-//			//Insert measurement types
-//			aux = 0;
-//			for (Integer mt_count : mt) {
-//				preamble += Downloads.FRAG_MT + aux;
-//				preamble += Downloads.FRAG_MEASUREMENT_TYPE_PART1;
-//				if (timeStampIndex != -1) {
-//					preamble += Downloads.FRAG_IN_DATE_TIME;
-//					preamble += Downloads.FRAG_IN_DATE_TIME_SUFFIX;
-//				}
-//				preamble += Downloads.FRAG_MEASUREMENT_TYPE_PART2;
-//				preamble += mt_count;
-//				preamble += Downloads.FRAG_MEASUREMENT_TYPE_PART3;
-//				preamble += "<" + p.getProperty(mt_count + "-entity") + ">"; 
-//				preamble += Downloads.FRAG_MEASUREMENT_TYPE_PART4;
-//				preamble += "<" + p.getProperty(mt_count + "-characteristic") + ">"; 
-//				preamble += Downloads.FRAG_MEASUREMENT_TYPE_PART5;
-//				preamble += "<" + p.getProperty(mt_count + "-unit") + ">"; 
-//				preamble += " .\n";
-//				aux++;
-//			}
-//
-//			if (timeStampIndex != -1) {
-//				preamble += "\n";
-//				preamble += Downloads.FRAG_IN_DATE_TIME_STATEMENT + " " + timeStampIndex + "  . \n";  
-//			}
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//			return ok (completeAnnotation.render("Error processing form. Please restart form."));
-//		} 
-//
-//		preamble += Downloads.FRAG_END_PREAMBLE;
-//	}
 }
