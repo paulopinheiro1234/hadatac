4f69a3cd86a9bc73c64ae1383b2295dc43318d39
Sabbir <rashidsabbir@gmail.com>
2016-09-15 10:56:24 -0400

    Added Login requirement for other faceted browser pages, cleaned up metadata acquisition page

--- app/org/hadatac/console/controllers/metadataacquisition/Pilot1.java
+++ app/org/hadatac/console/controllers/metadataacquisition/Pilot1.java
@@ -3,13 +3,19 @@ package org.hadatac.console.controllers.metadataacquisition;
 import play.Play;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import org.hadatac.console.controllers.AuthApplication;
 import org.hadatac.console.views.html.metadataacquisition.*;
 import org.hadatac.utils.Collections;
+
+import be.objectify.deadbolt.java.actions.Group;
+import be.objectify.deadbolt.java.actions.Restrict;
 
 
 public class Pilot1 extends Controller {
 	
 	// for /metadata HTTP GET requests
+	@Restrict(@Group(AuthApplication.DATA_OWNER_ROLE))
     public static Result index() {
     	String collection = Play.application().configuration().getString("hadatac.console.host_deploy") + 
     			request().path() + "/solrsearch";
@@ -19,6 +25,7 @@ public class Pilot1 extends Controller {
 
 
     // for /metadata HTTP POST requests
+    @Restrict(@Group(AuthApplication.DATA_OWNER_ROLE))
     public static Result postIndex() {
         return index();
         
--- app/org/hadatac/console/controllers/metadataacquisition/Pilot6.java
+++ app/org/hadatac/console/controllers/metadataacquisition/Pilot6.java
@@ -2,13 +2,19 @@ package org.hadatac.console.controllers.metadataacquisition;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import org.hadatac.console.controllers.AuthApplication;
 import org.hadatac.console.views.html.metadataacquisition.*;
 import org.hadatac.utils.Collections;
+
+import be.objectify.deadbolt.java.actions.Group;
+import be.objectify.deadbolt.java.actions.Restrict;
 
 
 public class Pilot6 extends Controller {
 	
 	// for /metadata HTTP GET requests
+	@Restrict(@Group(AuthApplication.DATA_OWNER_ROLE))
     public static Result index() {
        String collection = Collections.getCollectionsName(Collections.SUBJECTS_ACQUISITION);
        return ok(pilot6.render(collection));
@@ -17,6 +23,7 @@ public class Pilot6 extends Controller {
 
 
     // for /metadata HTTP POST requests
+	@Restrict(@Group(AuthApplication.DATA_OWNER_ROLE))
     public static Result postIndex() {
     	String collection = Collections.getCollectionsName(Collections.SUBJECTS_ACQUISITION);
         return ok(pilot6.render(collection));
