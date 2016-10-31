9fa6a371b2a81a628ce82166501e30fa7e583c38
Sabbir <rashidsabbir@gmail.com>
2016-10-31 17:33:38 -0400

    Made updates to subject and study view

--- app/org/hadatac/console/controllers/metadataacquisition/ViewSubject.java
+++ app/org/hadatac/console/controllers/metadataacquisition/ViewSubject.java
@@ -38,8 +38,33 @@ import be.objectify.deadbolt.java.actions.Restrict;
 
 public class ViewSubject extends Controller {
 	
-	public static Map<String, String> findBasic(String subject_uri) {
+	public static Map<String, List<String>> findBasic(String subject_uri) {
 
+		String subjectQueryString = "";
+		
+    	subjectQueryString = 
+    	"PREFIX sio: <http://semanticscience.org/resource/>" + 
+    	"PREFIX chear: <http://hadatac.org/ont/chear#>" + 
+    	"PREFIX chear-kb: <http://hadatac.org/kb/chear#>" + 
+    	"PREFIX prov: <http://www.w3.org/ns/prov#>" + 
+    	"PREFIX hasco: <http://hadatac.org/ont/hasco/>" + 
+    	"PREFIX hasneto: <http://hadatac.org/ont/hasneto#>" + 
+    	"PREFIX dcterms: <http://purl.org/dc/terms/>" + 
+    	"PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>" + 
+    	"PREFIX skos: <http://www.w3.org/2004/02/skos/core#>" +
+    	"PREFIX foaf: <http://xmlns.com/foaf/0.1/>" + 
+    	"SELECT ?subjectUri ?subjectTypeLabel ?subjectLabel ?cohortLabel ?studyLabel " +
+    	"			 WHERE {        ?subjectUri hasco:isSubjectOf* ?cohort . " +
+    	"							?study rdfs:label ?studyLabel ." +
+    	"			        		?cohort hasco:isCohortOf ?study . " +
+    	"							?cohort rdfs:label ?cohortLabel" +
+    	"			        		OPTIONAL { ?subjectUri rdfs:label ?subjectLabel } . " +
+    	"			        		OPTIONAL { ?subjectUri a ?subjectType . " +
+    	"										?subjectType rdfs:label ?subjectTypeLabel} . " +
+    	"			        		FILTER (?subjectUri = " + subject_uri + ") . " +
+    	"			                             }";
+		
+	/*	
 		String basicQueryString = "";
 
 		basicQueryString = 
@@ -48,16 +73,32 @@ public class ViewSubject extends Controller {
         + " PREFIX chear-kb: <http://hadatac.org/kb/chear#> "
         + "SELECT * "
         + "WHERE  {	" + subject_uri + " ?p ?o }";
-    	
+    */	
     	
         
-		Query basicQuery = QueryFactory.create(basicQueryString);
-		
+		//Query basicQuery = QueryFactory.create(basicQueryString);
+    	Query basicQuery = QueryFactory.create(subjectQueryString);
+    	
 		QueryExecution qexec = QueryExecutionFactory.sparqlService(Collections.getCollectionsName(Collections.METADATA_SPARQL), basicQuery);
 		ResultSet results = qexec.execSelect();
 		ResultSetRewindable resultsrw = ResultSetFactory.copyResults(results);
 		qexec.close();
 		
+		Map<String, List<String>> subjectResult = new HashMap<String, List<String>>();
+		List<String> values = new ArrayList<String>();
+		
+		while (resultsrw.hasNext()) {
+			QuerySolution soln = resultsrw.next();
+			System.out.println("HERE IS THE RAW SOLN*********" + soln.toString());
+			values = new ArrayList<String>();
+			values.add("Label: " + soln.get("subjectLabel").toString());
+			values.add("Type: " + soln.get("subjectTypeLabel").toString());
+			values.add("Cohort: " + soln.get("cohortLabel").toString());
+			values.add("Study: " + soln.get("studyLabel").toString());
+			subjectResult.put(soln.get("subjectUri").toString(),values);	
+			System.out.println("THIS IS SUBROW*********" + subjectResult);	
+		}
+/*		
 		Map<String, String> poResult = new HashMap<String, String>();
 		System.out.println("HERE IS THE RAW resultsrw*********" + resultsrw);
 		while (resultsrw.hasNext()) {
@@ -66,8 +107,8 @@ public class ViewSubject extends Controller {
 			poResult.put(soln.get("p").toString(), soln.get("o").toString());
 //			System.out.println("THIS IS SUBROW*********" + poResult);
 
-		}
-		return poResult;
+		}*/
+		return subjectResult;
 	}
 	
 	public static List<String> findSample(String subject_uri) {
@@ -94,7 +135,7 @@ public class ViewSubject extends Controller {
 			QuerySolution soln = resultsrw.next();
 			System.out.println("HERE IS THE SAMPLES*********" + soln.toString());
 			sampleResult.add(soln.get("s").toString());
-//			System.out.println("THIS IS SUBROW*********" + poResult);
+			System.out.println("THIS IS SUBROW*********" + sampleResult);
 
 		}
 		return sampleResult;
@@ -104,10 +145,10 @@ public class ViewSubject extends Controller {
 	@Restrict(@Group(AuthApplication.DATA_OWNER_ROLE))
     public static Result index(String subject_uri) {
 
-    	Map<String, String> poResult = findBasic(subject_uri);
+    	Map<String, List<String>> subjectResult = findBasic(subject_uri);
     	List<String> sampleResult = findSample(subject_uri);
         
-    	return ok(viewSubject.render(poResult,sampleResult));
+    	return ok(viewSubject.render(subjectResult,sampleResult));
     
         
     }// /index()
