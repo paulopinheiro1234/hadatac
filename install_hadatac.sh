f7fa809548f2c4e63d65626325a08b6194587969
jason <jason@Jasons-MacBook-Pro.local>
2017-01-23 12:34:58 -0500

    Removed solr4, solrrdf and corresponding installation scripts

--- install_hadatac.sh
+++ install_hadatac.sh
@@ -31,7 +31,6 @@ fi
 
 HADATAC_DOWNLOAD=$HADATAC_HOME/download
 HADATAC_SOLR=$HADATAC_HOME/solr
-SOLR4_HOME=$HADATAC_SOLR/solr-4.10.4
 SOLR5_HOME=$HADATAC_SOLR/solr-5.2.1
 
 mkdir $HADATAC_HOME
@@ -40,11 +39,6 @@ mkdir $HADATAC_SOLR
 
 cp -R * $HADATAC_HOME
 
-echo "=== Downloading Apache Solr 4.10.4..."
-wget -O $HADATAC_DOWNLOAD/solr-4.10.4.tgz http://archive.apache.org/dist/lucene/solr/4.10.4/solr-4.10.4.tgz
-wait $!
-wget -O $HADATAC_DOWNLOAD/solr-4.10.4.tgz.md5 http://archive.apache.org/dist/lucene/solr/4.10.4/solr-4.10.4.tgz.md5
-wait $!
 echo "=== Downloading Apache Solr 5.2.1..."
 wget -O $HADATAC_DOWNLOAD/solr-5.2.1.tgz http://archive.apache.org/dist/lucene/solr/5.2.1/solr-5.2.1.tgz
 wait $!
@@ -53,23 +47,12 @@ wait $!
 echo "=== Downloading JTS Topology Suite 1.13..."
 wget -O $HADATAC_DOWNLOAD/jts-1.13.zip http://pilotfiber.dl.sourceforge.net/project/jts-topo-suite/jts/1.13/jts-1.13.zip
 
-echo "=== Uncompressing Apache Solr 4.10.4..."
-tar xfz $HADATAC_DOWNLOAD/solr-4.10.4.tgz -C $HADATAC_SOLR
-wait $!
 echo "=== Uncompressing Apache Solr 5.2.1..."
 tar xfz $HADATAC_DOWNLOAD/solr-5.2.1.tgz -C $HADATAC_SOLR
 wait $!
 echo "=== Uncompressing JTS Topology Suite 1.13..."
 unzip -o -qq $HADATAC_DOWNLOAD/jts-1.13.zip -d $HADATAC_DOWNLOAD/jts-1.13
 wait $!
-
-echo "HADATAC_SOLR=$HADATAC_SOLR" >> $HADATAC_SOLR/hadatac_solr.sh
-cat $HADATAC_SOLR/solr4.in.sh >> $HADATAC_SOLR/hadatac_solr.sh
-mv $HADATAC_SOLR/hadatac_solr.sh $HADATAC_SOLR/solr4.in.sh
-
-echo "HADATAC_SOLR=$HADATAC_SOLR" >> $HADATAC_SOLR/hadatac_solr.sh
-cat $HADATAC_SOLR/run_solr4.sh >> $HADATAC_SOLR/hadatac_solr.sh
-mv $HADATAC_SOLR/hadatac_solr.sh $HADATAC_SOLR/run_solr4.sh
 
 echo "HADATAC_SOLR=$HADATAC_SOLR" >> $HADATAC_SOLR/hadatac_solr.sh
 cat $HADATAC_SOLR/solr5.in.sh >> $HADATAC_SOLR/hadatac_solr.sh
@@ -79,14 +62,11 @@ echo "HADATAC_SOLR=$HADATAC_SOLR" >> $HADATAC_SOLR/hadatac_solr.sh
 cat $HADATAC_SOLR/run_solr5.sh >> $HADATAC_SOLR/hadatac_solr.sh
 mv $HADATAC_SOLR/hadatac_solr.sh $HADATAC_SOLR/run_solr5.sh
 
-sh $HADATAC_SOLR/run_solr4.sh start
-wait $!
 sh $HADATAC_SOLR/run_solr5.sh start
 wait $!
 
 cp $HADATAC_DOWNLOAD/jts-1.13/lib/* $HADATAC_SOLR/solr-5.2.1/server/solr-webapp/webapp/WEB-INF/lib/
 
-sh $HADATAC_SOLR/run_solr4.sh restart
 sh $HADATAC_SOLR/run_solr5.sh restart
 
 
--- solr/solr4.in.bat
+++
@@ -1,5 +0,0 @@
-set SOLR_PID_DIR=%HADATAC_SOLR%\solr-4.10.4
-set SOLR_HOME=%HADATAC_SOLR%\solrdf-home
-set LOG4J_PROPS=%HADATAC_SOLR%\solr-4.10.4\example\resources\log4j.properties
-set SOLR_LOGS_DIR=%HADATAC_SOLR%\solr-4.10.4\example\logs
-set SOLR_PORT=7574
