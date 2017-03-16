cf20cdd06482931c839e4a970b443d93ab55c854
Sabbir <rashidsabbir@gmail.com>
2017-03-16 10:47:44 -0400

    Updated sample generator to read most values from input csv

--- app/org/hadatac/data/loader/GenerateSamples.java
+++ app/org/hadatac/data/loader/GenerateSamples.java
@@ -1,58 +1,38 @@
 package org.hadatac.data.loader;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
 import java.lang.String;
 
 public class GenerateSamples {
 
     public static void main(String[] args) throws IOException {
-        
-        int pilotNum=6;
-        String kbPrefix="chear-kb:";
-        String inputFile="sampleList.csv";
+        String inputFile="SID.csv";
         String outputFile="samples.tsv";
-        BufferedWriter bw = null;
-
-        System.out.println("About to Read in file");
-        Scanner s = new Scanner(new File(inputFile));
-        ArrayList<String> sampleIDs = new ArrayList<String>();
-        while (s.hasNext()){
-            sampleIDs.add(s.next());
-        }
-        s.close();
-        
-        int numSamples=sampleIDs.size();
-        // Pilot 6 Sample Structure
-        String samplingVolUnit = "obo:UO_0000095"; //"uo:ml"
-        int storageTemp = -80;
+        String line = "";
+        String comma = ",";
+        int counter=0;
+        int pilotNum;
+        String samplingVolUnit = ""; //"obo:UO_0000095"; //"uo:ml"
+        String storageTemp; //-80;
         String storageTempUnit = "obo:UO_0000027"; //"uo:degree Celsius"
-        //DNA Isolation
-        int FTcount = 0;
-        String sampleType = "chear:WholeBlood";
-        String objectOf = "chear-kb:DA-PS6-DNAIsolation";
-        float samplingVol = 8.5f;
+        String FTcount; // = 0;
+        String sampleType = "";// = "chear:WholeBlood";
+        String objectOf = ""; // = "chear-kb:DA-PS6-DNAIsolation";
+        //float samplingVol; // = 8.5f;
+        String samplingVol;
         String samplingMethod = ""; //"PaxGene Blood DNA";
-        String sampleSuffix = "Whole_Blood-DNA_Isolation";
-/*        //Untargeted Metabolomics and Lipidomics
-        int FTcount = 1;
-        String sampleType = "chear:BloodPlasma";
-        String objectOf = "chear-kb:DA-PS6-UML";
-        float samplingVol = 0.0f;
-        String samplingMethod = ""; //null
-        String sampleSuffix = "Plasma-UML";
-        //ICP-MS
-        int FTcount = 1;
-        String sampleType = "chear:Urine";
-        String objectOf = "chear-kb:DA-PS6-ICP-MS";
-        float samplingVol = 0.0f;
-        String samplingMethod = ""; //null
-        String sampleSuffix = "Urine-ICP-MS";
-*/
+        String sampleSuffix = "";// "Whole_Blood-DNA_Isolation";
+        String sampleID = "";
+        String subjectID = "";
+        String kbPrefix="chear-kb:";
+        BufferedWriter bw = null;
+        BufferedReader br = new BufferedReader(new FileReader(inputFile));
+        
         try {
         	File file = new File(outputFile);
         	if (!file.exists()) {
@@ -61,34 +41,83 @@ public class GenerateSamples {
     	    FileWriter fw = new FileWriter(file);
     	    bw = new BufferedWriter(fw);
             bw.write("hasURI\ta\trdfs:label\thasco:originalID\thasco:isSampleOf\thasco:isMeasuredObjectOf\trdfs:comment\thasco:hasSamplingMethod\thasco:hasSamplingVolume\thasco:hasSamplingVolumeUnit\thasco:hasStorageTemperature\thasco:hasStorageTemperatureUnit\thasco:hasNumFreezeThaw\n");
-            for (int i=0;i<numSamples;i++) {
-                //hasURI
-                bw.write(kbPrefix + "SPL-" + String.format("%04d", i+1) + "-Pilot-" + pilotNum + "-" + sampleSuffix + "\t");
-                //rdf:type                
-                bw.write(sampleType + "\t");
-                //rdfs:label
-                bw.write("SID " + String.format("%04d", i+1) + " - Pilot " + pilotNum + " " + sampleSuffix + "\t");
-                //hasco:originalID
-                bw.write(sampleIDs.get(i) + "\t");
-                //hasco:isSampleOf
-                bw.write(kbPrefix + "SBJ-" + String.format("%04d", i+1) + "-Pilot-" + pilotNum + "\t");
-                // hasco:isMeasuredObjectOf
-                bw.write(objectOf + "\t");
-                // rdfs:comment
-                bw.write("Sample " + String.format("%04d", i+1) + " for Pilot " + pilotNum + " " + sampleSuffix + "\t");
-                // hasco:hasSamplingMethod
-                bw.write(samplingMethod + "\t");
-                //hasco:hasSamplingVolume
-                bw.write(String.format("%1.2f", samplingVol) + "\t");
-                //hasco:hasSamplingVolumeUnit
-                bw.write(samplingVolUnit + "\t");
-                //hasco:hasStorageTemperature
-                bw.write(storageTemp + "\t");
-                //hasco:hasStorageTemperatureUnit
-                bw.write(storageTempUnit + "\t");
-                //hasNumFreezeThaw
-                bw.write(FTcount + "\n");
-            }
+            //try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+                while ((line = br.readLine()) != null) {
+                    // use comma as separator
+                    String[] columns = line.split(comma);
+                    if(counter==0){
+                        /*System.out.println("columns: " + columns);  
+                        System.out.println("columns[0]: " + columns[0]); 
+                        System.out.println("columns[1]: " + columns[1]); 
+                        System.out.println("columns[2]: " + columns[2]); 
+                        System.out.println("columns[3]: " + columns[3]); 
+                        System.out.println("columns[4]: " + columns[4]); 
+                        System.out.println("columns[5]: " + columns[5]); 
+                        System.out.println("columns[6]: " + columns[6]); 
+                        System.out.println("columns[7]: " + columns[7]); 
+                        System.out.println("columns[8]: " + columns[8]);   
+                        System.out.println("columns[9]: " + columns[9]); 
+                        System.out.println("columns[10]: " + columns[10]); 
+                        System.out.println("columns[11]: " + columns[11]); 
+                        System.out.println("columns[12]: " + columns[12]);   
+                        System.out.println("columns[13]: " + columns[13]); 
+                        System.out.println("columns[14]: " + columns[14]); 
+                        System.out.println("columns[15]: " + columns[15]); 
+                        System.out.println("columns[16]: " + columns[16]);    
+                        System.out.println("columns[17]: " + columns[17]); 
+                        System.out.println("columns[18]: " + columns[18]); 
+                        System.out.println("columns[19]: " + columns[19]); 
+                        System.out.println("columns[20]: " + columns[20]);   
+                        System.out.println("columns[21]: " + columns[21]); 
+                        System.out.println("columns[22]: " + columns[22]); 
+                        System.out.println("columns[23]: " + columns[23]); 
+                        System.out.println("columns[24]: " + columns[24]);  
+                        System.out.println("columns[25]: " + columns[25]);   
+                        System.out.println("columns[26]: " + columns[26]); 
+                        System.out.println("columns[27]: " + columns[27]); 
+                        System.out.println("columns[28]: " + columns[28]); 
+                        System.out.println("columns[29]: " + columns[29]);  */   
+                    } else {
+                        sampleID = columns[0];
+                        sampleSuffix = columns[3];
+//                        kbPrefix = columns[6] + ":";
+                        subjectID = columns[7];
+                        pilotNum = Integer.parseInt(columns[8]);
+                        sampleType = columns[11];
+                        samplingMethod = columns[12];
+                        samplingVol = columns[15];
+                        samplingVolUnit = columns[16];
+                        storageTemp = columns[18];
+                        FTcount = columns[19];
+                        //hasURI
+                        bw.write(kbPrefix + "SPL-" + String.format("%04d", counter) + "-Pilot-" + pilotNum + "-" + sampleSuffix + "\t");
+                        //rdf:type                
+                        bw.write(sampleType + "\t");
+                        //rdfs:label
+                        bw.write("SID " + String.format("%04d", counter) + " - Pilot " + pilotNum + " " + sampleSuffix + "\t");
+                        //hasco:originalID
+                        bw.write(sampleID + "\t");
+                        //hasco:isSampleOf
+                        bw.write(kbPrefix + "SBJ-" + String.format("%04d", counter) + "-" + subjectID + "-Pilot-" + pilotNum + "\t");
+                        // hasco:isMeasuredObjectOf
+                        bw.write(objectOf + "\t");
+                        // rdfs:comment
+                        bw.write("Sample " + String.format("%04d", counter) + " for Pilot " + pilotNum + " " + sampleSuffix + "\t");
+                        // hasco:hasSamplingMethod
+                        bw.write(samplingMethod + "\t");
+                        //hasco:hasSamplingVolume
+                        bw.write(samplingVol + "\t");
+                        //hasco:hasSamplingVolumeUnit
+                        bw.write(samplingVolUnit + "\t");
+                        //hasco:hasStorageTemperature
+                        bw.write(storageTemp + "\t");
+                        //hasco:hasStorageTemperatureUnit
+                        bw.write(storageTempUnit + "\t");
+                        //hasNumFreezeThaw
+                        bw.write(FTcount + "\n");
+                    }              
+                    counter++;
+                }
             System.out.println("File written Successfully");
         } catch (IOException ioe) {
             ioe.printStackTrace();
@@ -101,7 +130,12 @@ public class GenerateSamples {
     	    }catch(Exception ex){
     	        System.out.println("Error in closing the BufferedWriter: " + ex);
 	        }
+    	    try{ 
+     	       if(br!=null)
+         		   br.close();
+     	    }catch(Exception ex){
+     	        System.out.println("Error in closing the BufferedReader: " + ex);
+ 	        }
     	}
     }
-
 }
