b3bab251d2931df635e3b7554b252a444137dc75
Yue Liu (Robin) <liuyue0027@gmail.com>
2017-03-29 18:12:51 -0400

    Update to batch scripts

---
+++ solr/solr6.in.bat
@@ -0,0 +1,5 @@
+set SOLR_PID_DIR=%HADATAC_SOLR%\solr-6.5.0
+set SOLR_HOME=%HADATAC_SOLR%\solr-home
+set LOG4J_PROPS=%HADATAC_SOLR%\solr-6.5.0\server\resources\log4j.properties
+set SOLR_LOGS_DIR=%HADATAC_SOLR%\solr-6.5.0\server\logs
+set SOLR_PORT=8983
