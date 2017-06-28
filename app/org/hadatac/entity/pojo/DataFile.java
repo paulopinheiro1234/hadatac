c2869e85f43f77878353fb2a31864167b9e84724
Paulo Pinheiro <pp3223@gmail.com>
2017-06-27 22:08:38 -0400

    generic and chear template configuration file

--- app/org/hadatac/entity/pojo/DataFile.java
+++ app/org/hadatac/entity/pojo/DataFile.java
@@ -57,7 +57,7 @@ public class DataFile {
 		return dataAcquisitionUri;
 	}
 	public void setDataAcquisitionUri(String dataAcquisitionUri) {
-         	System.out.println("inside DataFile's POJO DA: <" + dataAcquisitionUri + ">");
+	        //System.out.println("inside DataFile's POJO DA: <" + dataAcquisitionUri + ">");
 		this.dataAcquisitionUri = dataAcquisitionUri;
 	}
 	
@@ -72,7 +72,7 @@ public class DataFile {
 		return fileName;
 	}
 	public void setFileName(String fileName) {
-         	System.out.println("inside DataFile's POJO: " + fileName);
+	        //System.out.println("inside DataFile's POJO: " + fileName);
 		this.fileName = fileName;
 	}
 	
