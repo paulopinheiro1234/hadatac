95751bd4b242e7fa4d29999d1dd91b120cefc181
jasonliang <gychant@qq.com>
2017-08-14 23:38:26 -0400

    Pop up error webpage when solr is down

--- app/org/hadatac/console/controllers/Portal.java
+++ app/org/hadatac/console/controllers/Portal.java
@@ -4,49 +4,31 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import play.twirl.api.Html;
 
-import javax.xml.ws.http.HTTPException;
-
-import org.apache.jena.query.Query;
-import org.apache.jena.query.QueryExecution;
-import org.apache.jena.query.QueryExecutionFactory;
-import org.apache.jena.query.QueryFactory;
-import org.apache.jena.sparql.engine.http.QueryExceptionHTTP;
 import org.hadatac.console.views.html.main;
 import org.hadatac.console.views.html.portal;
-import org.hadatac.utils.Collections;
+import org.hadatac.utils.Repository;
 
 public class Portal extends Controller {
 	
     public static Result index() {
-    	if (!checkTripleStoreConnection()) {
+    	if (!Repository.operational(Repository.METADATA)) {
     		return ok(main.render("Results", "", 
-    				new Html("<div><h4>The triple store is NOT properly connected. "
-    						+ "Please restart it or check the hadatac configuration file!</h4></div>")));
+    				new Html("<div class=\"container-fluid\"><h4>"
+    						+ "The triple store is NOT properly connected. "
+    						+ "Please restart it or check the hadatac configuration file!"
+    						+ "</h4></div>")));
     	}
+    	if (!Repository.operational(Repository.DATA)) {
+    		return ok(main.render("Results", "", 
+    				new Html("<div class=\"container-fluid\"><h4>"
+    						+ "HADatAC Solr is down now. Ask Administrator for further information. "
+    						+ "</h4></div>")));
+    	}
+    	
     	return ok(portal.render()); 
     }
 
     public static Result postIndex() {
     	return ok(portal.render());
-    }
-
-    private static boolean checkTripleStoreConnection() {
-    	String queryString = "SELECT (COUNT(*) as ?count) WHERE { ?s ?p ?o . } ";
-		Query query = QueryFactory.create(queryString);
-		
-		QueryExecution qe;
-		try {
-			qe = QueryExecutionFactory.sparqlService(
-					Collections.getCollectionsName(Collections.METADATA_SPARQL), query);
-			qe.execSelect();
-			
-			qe = QueryExecutionFactory.sparqlService(
-					Collections.getCollectionsName(Collections.PERMISSIONS_SPARQL), query);
-			qe.execSelect();
-		} catch (QueryExceptionHTTP e) {
-			return false;
-		}
-		
-		return true;
     }
 }
