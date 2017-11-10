8e085ce99c07365bc3ddf4ca560405fd8e839774
chastk <chastk@rpi.edu>
2017-11-10 14:30:08 -0500

    intermediate commit

--- app/org/hadatac/data/loader/ccsv/Parser.java
+++ app/org/hadatac/data/loader/ccsv/Parser.java
@@ -532,13 +532,13 @@ public class Parser {
 				// TODO: un-hack this
 				// HACK FOR AUGUST 18
 				// HEAD
-				//measurement.setEntity(rowInstances.get(dasa.getObjectUri()).getLabel()); 
-				//measurement.setEntityUri(dasa.getEntity());
-				//System.out.println("[Parser] getEntity " + dasa.getEntity());
+				measurement.setEntity(rowInstances.get(dasa.getObjectUri()).getLabel()); 
+				measurement.setEntityUri(dasa.getEntity());
+				System.out.println("[Parser] getEntity " + dasa.getEntity());
 
 				// ORIGIN
-				measurement.setEntity("STUDY OBJECT");
-				measurement.setEntityUri(StudyObject.findUribyOriginalId(measurement.getValue()));
+				//measurement.setEntity("STUDY OBJECT");
+				//measurement.setEntityUri(StudyObject.findUribyOriginalId(measurement.getValue()));
 
 				measurement.setCharacteristic(uppercaseFirstLetter(dasa.getAttributeLabel()));
 				measurement.setCharacteristicUri(dasa.getAttribute());
