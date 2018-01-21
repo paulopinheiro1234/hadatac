0132ef8434d40e72a0a76e1809e92d24d63134ee
jasonliang <gychant@qq.com>
2018-01-20 20:42:22 -0500

    Remove deprecated functional calls

--- app/org/hadatac/console/controllers/dataanalysis/NewFunction.java
+++ app/org/hadatac/console/controllers/dataanalysis/NewFunction.java
@@ -3,37 +3,36 @@ package org.hadatac.console.controllers.dataanalysis;
 import org.hadatac.console.views.html.dataanalysis.*;
 
 import java.util.List;
-import java.util.Map;
 
 import org.hadatac.console.controllers.AuthApplication;
-import org.hadatac.console.controllers.indicators.routes;
 import org.hadatac.console.controllers.metadata.DynamicFunctions;
 import org.hadatac.console.models.IndicatorForm;
 import org.hadatac.console.models.SysUser;
 import org.hadatac.entity.pojo.Aggregate;
-import org.hadatac.entity.pojo.Attribute;
-import org.hadatac.entity.pojo.Characteristic;
-import org.hadatac.entity.pojo.Entity;
 import org.hadatac.entity.pojo.Indicator;
-import org.hadatac.entity.pojo.Unit;
 import org.hadatac.entity.pojo.facet.EntityCharacteristic;
 import org.hadatac.metadata.loader.ValueCellProcessing;
+
+import com.google.inject.Inject;
 
 import be.objectify.deadbolt.java.actions.Group;
 import be.objectify.deadbolt.java.actions.Restrict;
 import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class NewFunction extends Controller {
+	
+	@Inject
+	FormFactory formFactory;
+	
 	@Restrict(@Group(AuthApplication.DATA_OWNER_ROLE))
     public Result index() {
 		final SysUser sysUser = AuthApplication.getLocalUser(session());
 		
     	// may need addressing
     	Indicator indicator = new Indicator();
-    	List<Attribute> attributes = Attribute.find();
-    	List<Unit> units = Unit.find();
     	List<Aggregate> aggregates = Aggregate.find();
     	EntityCharacteristic entityCharacteristic = EntityCharacteristic.create(sysUser.getUri());
     	return ok(newFunction.render(indicator, entityCharacteristic, aggregates));
@@ -46,9 +45,8 @@ public class NewFunction extends Controller {
 	
 	@Restrict(@Group(AuthApplication.DATA_OWNER_ROLE))
     public Result processForm() {
-    	final SysUser sysUser = AuthApplication.getLocalUser(session());
-	
-        Form<IndicatorForm> form = Form.form(IndicatorForm.class).bindFromRequest();
+		
+        Form<IndicatorForm> form = formFactory.form(IndicatorForm.class).bindFromRequest();
         IndicatorForm data = form.get();
         
         if (form.hasErrors()) {
