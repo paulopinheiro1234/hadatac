4dca3a49d6145765e5a085dd33ab60995ad9abcd
Yue Liu (Robin) <liuyue0027@gmail.com>
2018-02-08 15:01:59 -0500

    Add more templates to triple deletion

--- app/org/hadatac/console/controllers/annotator/AutoAnnotator.java
+++ app/org/hadatac/console/controllers/annotator/AutoAnnotator.java
@@ -316,12 +316,22 @@ public class AutoAnnotator extends Controller {
 				String query = "";
 				query += NameSpaces.getInstance().printSparqlNameSpaceList();
 				query += "DELETE WHERE {  ";
-					query += str + " ?p ?o . ";
+				query += str + " ?p ?o . ";
 				query += "}  ";				
 //				System.out.println(query);
 				UpdateRequest request = UpdateFactory.create(query);
 				UpdateProcessor processor = UpdateExecutionFactory.createRemote(request, Collections.getCollectionsName(Collections.METADATA_UPDATE));
 				processor.execute();
+				
+				String query2 = "";
+				query2 += NameSpaces.getInstance().printSparqlNameSpaceList();
+				query2 += "DELETE WHERE {  ";
+				query2 += "?s ?p " + str + " . ";
+				query2 += "}  ";
+//				System.out.println(query);
+				UpdateRequest request2 = UpdateFactory.create(query2);
+				UpdateProcessor processor2 = UpdateExecutionFactory.createRemote(request2, Collections.getCollectionsName(Collections.METADATA_UPDATE));
+				processor2.execute();
 			}
 		}
 	}
