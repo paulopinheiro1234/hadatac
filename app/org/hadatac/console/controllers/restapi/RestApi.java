2758ca594feb370694816cd844057639000a1191
chastk <chastk@rpi.edu>
2018-03-13 15:24:17 -0400

    updates to REST api

--- app/org/hadatac/console/controllers/restapi/RestApi.java
+++ app/org/hadatac/console/controllers/restapi/RestApi.java
@@ -92,47 +92,15 @@ public class RestApi extends Controller {
         }
     }// /getVariable()
 
-    // TODO: finish this
     // test with http%3A%2F%2Fhadatac.org%2Fkb%2Fhbgd%23STD-CPP4
     // The DASchema will return ALL variables in the schema for the given study
     // We need to hit solr to make sure we only get variables for which there exist measurements
     //@Restrict(@Group(AuthApplication.DATA_OWNER_ROLE))
     public Result getVariablesInStudy(String studyUri){
-        // Step 1: query solr to see for what variables we have measurements
-        //         and return the URI's for those variables
-        SolrQuery solrQuery = new SolrQuery();
-        // restrict to the study provided by parameter
-        solrQuery.setQuery("study_uri_str:\"" + studyUri + "\"");
-        //System.out.println("[GetVarsInStudy] query = " + query.getQuery());
-        solrQuery.setRows(0);
-        solrQuery.setFacet(true);
-        solrQuery.setFacetLimit(-1);
-        solrQuery.setParam("json.facet", "{ "
-                + "vars:{ "
-                + "type: terms, "
-                + "field: characteristic_uri_str, "
-            // limit is important - the default if this is left out is only 10 facets
-                + "limit: 1000}}");
-        List<String> uris = new ArrayList<String>();
-
-        try {
-            SolrClient solr = new HttpSolrClient.Builder(
-                    ConfigFactory.load().getString("hadatac.solr.data") 
-                    + CollectionUtil.DATA_ACQUISITION).build();
-            QueryResponse queryResponse = solr.query(solrQuery, SolrRequest.METHOD.POST);
-            solr.close();
-            Pivot pivot = Pivot.parseQueryResponse(queryResponse);
-            uris = parsePivotForVars(pivot);
-        } catch (Exception e) {
-            System.out.println("[ERROR] RestApi.getVariablesByStudy() - Exception message: " + e.getMessage());
-            return internalServerError(ApiUtil.createResponse("Encountered SOLR error!", false));
+        List<String> uris = getUsedVarsInStudy(studyUri);
+        if(uris == null){
+            return notFound(ApiUtil.createResponse("Encountered SOLR error!", false));
         }
-
-        if (uris.isEmpty()){
-            return notFound(ApiUtil.createResponse("No variables found in solr for study " + studyUri, false));
-        }
-        System.out.println("[GetVarsInStudy] Found " + uris.size() + " variables for this study");
-
 
         // Step 2: query blazegraph with those URI's to get details of those vars
         //         and return the results with those details as a RESTful response
@@ -142,8 +110,9 @@ public class RestApi extends Controller {
         }
 
         String sparqlQueryString = NameSpaces.getInstance().printSparqlNameSpaceList() + 
-                "select ?class ?label ?unit where {" + 
+                "select distinct ?class ?label ?unit ?shortname where {" + 
                 "?class rdfs:label ?label ." +
+                "?class dcterms:identifier ?shortname ." + 
                 "?dasa hasco:hasAttribute ?class ." + 
                 "?dasa hasco:hasUnit ?unit . " + 
                 "} values ?class { " + classes + "} ";
@@ -178,6 +147,9 @@ public class RestApi extends Controller {
 			if (soln.get("unit") != null) {
 				temp.put("unit", soln.get("unit").toString());
 			}
+			if (soln.get("shortname") != null) {
+				temp.put("shortname", soln.get("shortname").toString());
+			}
             anode.add(temp);
         }// /parse sparql results
         System.out.println("[GetVarsInStudy] parsed " + anode.size() + " results into array");
@@ -186,6 +158,100 @@ public class RestApi extends Controller {
         System.out.println("[GetVarsInStudy] done");
         return ok(ApiUtil.createResponse(jsonObject, true));
     }// /getVariablesInStudy
+
+
+    // This method takes the same solr query as above,
+    // but instead queries the triple store for units instead of attributes
+    public Result getUnitsInStudy(String studyUri){
+        List<String> uris = getUsedVarsInStudy(studyUri);
+        if(uris == null){
+            return notFound(ApiUtil.createResponse("Encountered SOLR error!", false));
+        }
+
+        // Step 2: query blazegraph with those URI's to get details of those vars
+        //         and return the results with those details as a RESTful response
+        String classes = "";
+        for (String s : uris) {
+            classes += "<" + s + "> ";
+        }
+
+        String sparqlQueryString = NameSpaces.getInstance().printSparqlNameSpaceList() + 
+                "select distinct ?uri ?label where {" + 
+                "?dasa hasco:hasAttribute ?class ." + 
+                "?dasa hasco:hasUnit ?uri . " + 
+                "?uri rdfs:label ?label ." +
+                "} values ?class { " + classes + "} ";
+        //System.out.println("[GetVarsInStudy] sparql query\n" + sparqlQueryString);
+		
+		Query sparqlQuery = QueryFactory.create(sparqlQueryString);
+		QueryExecution qexec = QueryExecutionFactory.sparqlService(
+				CollectionUtil.getCollectionsName(CollectionUtil.METADATA_SPARQL), sparqlQuery);
+		ResultSet results = qexec.execSelect();
+		ResultSetRewindable resultsrw = ResultSetFactory.copyResults(results);
+		qexec.close();
+
+        if(resultsrw.size() == 0){
+            return notFound(ApiUtil.createResponse("No units found in blazegraph for study " + studyUri, false));
+        }
+        
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode anode = mapper.createArrayNode();
+
+        while(resultsrw.hasNext()){
+            ObjectNode temp = mapper.createObjectNode();
+			QuerySolution soln = resultsrw.next();
+			if (soln.get("uri") != null) {
+				temp.put("uri", soln.get("uri").toString());
+			} else {
+                System.out.println("[GetUnitsInStudy] ERROR: Result returned without URI? Skipping....");
+                continue;
+            }
+			if (soln.get("label") != null) {
+				temp.put("label", soln.get("label").toString());
+			}
+            anode.add(temp);
+        }// /parse sparql results
+        System.out.println("[GetUnitsInStudy] parsed " + anode.size() + " results into array");
+
+        JsonNode jsonObject = mapper.convertValue(anode, JsonNode.class);
+        System.out.println("[GetUnitsInStudy] done");
+        return ok(ApiUtil.createResponse(jsonObject, true));
+    }// /getVariablesInStudy
+
+
+    // query solr to see for what variables we have measurements
+    // and return the URI's for those variables
+    private List<String> getUsedVarsInStudy(String studyUri){
+        SolrQuery solrQuery = new SolrQuery();
+        // restrict to the study provided by parameter
+        solrQuery.setQuery("study_uri_str:\"" + studyUri + "\"");
+        //System.out.println("[GetVarsInStudy] query = " + query.getQuery());
+        solrQuery.setRows(0);
+        solrQuery.setFacet(true);
+        solrQuery.setFacetLimit(-1);
+        solrQuery.setParam("json.facet", "{ "
+                + "vars:{ "
+                + "type: terms, "
+                + "field: characteristic_uri_str, "
+            // limit is important - the default if this is left out is only 10 facets
+                + "limit: 1000}}");
+        List<String> uris = new ArrayList<String>();
+
+        try {
+            SolrClient solr = new HttpSolrClient.Builder(
+                    ConfigFactory.load().getString("hadatac.solr.data") 
+                    + CollectionUtil.DATA_ACQUISITION).build();
+            QueryResponse queryResponse = solr.query(solrQuery, SolrRequest.METHOD.POST);
+            solr.close();
+            Pivot pivot = Pivot.parseQueryResponse(queryResponse);
+            uris = parsePivotForVars(pivot);
+        } catch (Exception e) {
+            System.out.println("[ERROR] RestApi.getUsedVarsInStudy() - Exception message: " + e.getMessage());
+            return null;
+        }
+        System.out.println("[getUsedVarsInStudy] Found " + uris.size() + " variables for this study");
+        return uris;
+    }// /getUsedVarsInStudy()
 
     private List<String> parsePivotForVars (Pivot p) {
         List<String> results = new ArrayList<String>();
