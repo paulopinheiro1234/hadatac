4a07e34c35d944be12111de9c9e0056cc452bc45
jasonliang <gychant@qq.com>
2018-05-19 09:09:45 -0400

    Added spreedsheet preview using SheetJS

---
+++ public/sheetjs/js/sheetjsw.js
@@ -0,0 +1,8 @@
+importScripts('//unpkg.com/xlsx/dist/xlsx.full.min.js');
+postMessage({t:'ready'});
+onmessage = function(evt) {
+  var v;
+  try { v = XLSX.read(evt.data.d, evt.data.b); }
+  catch(e) { postMessage({t:"e",d:e.stack}); }
+  postMessage({t:evt.data.t, d:JSON.stringify(v)});
+}
