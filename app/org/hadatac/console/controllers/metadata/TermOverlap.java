e0346b05e715df72ea4fee0df12b80c57d13aa47
Matthew Johnson <johnsm21@rpi.edu>
2018-10-16 20:38:56 -0400

    Added Overlapping terms view that clusters simalar ontology terms at http://localhost:9000/hadatac/metadata/termOverlap

---
+++ app/org/hadatac/console/controllers/metadata/TermOverlap.java
@@ -0,0 +1,39 @@
+package org.hadatac.console.controllers.metadata;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import java.util.List;
+import java.util.Map;
+
+import org.hadatac.console.views.html.metadata.*;
+import org.hadatac.console.controllers.schema.EditingOptions;
+import org.hadatac.console.models.ForceFieldQuery;
+import org.hadatac.console.models.OtMSparqlQueryResults;
+import org.hadatac.utils.Hierarchy;
+import org.hadatac.utils.OntologyTerm;
+import org.hadatac.utils.TermClusteror;
+
+public class TermOverlap extends Controller {
+
+	// for /metadata HTTP GET requests
+    public Result index() {
+    
+    	Map<String, List<OntologyTerm>> t = new TermClusteror().getTermClusters();
+        System.out.println("TermOverlap index() was called!");
+        return ok(searchTermOverlap.render(t));
+        
+    }// /index()
+
+
+    // for /metadata HTTP POST requests
+    public Result postIndex() {
+        
+    	
+    	Map<String, List<OntologyTerm>> t = new TermClusteror().getTermClusters();
+		
+        System.out.println("TermOverlap index() was called!");
+        return ok(searchTermOverlap.render(t));
+        
+    }// /postIndex()
+}
