052b64d02bbbe5a9daa5b3c4215abdd09e32ed35
Sun Yutao <sunyu.tt@163.com>
2019-07-10 00:02:07 -0400

    load all ontologies in UploadOntologyTest

--- test/ingestDataTest/UploadOntologyTest.java
+++ test/ingestDataTest/UploadOntologyTest.java
@@ -14,11 +14,24 @@ import java.util.List;
 import org.hadatac.console.controllers.triplestore.LoadOnt;
 import org.hadatac.metadata.loader.MetadataContext;
 import org.hadatac.utils.Feedback;
+import org.hadatac.utils.NameSpace;
 import org.hadatac.utils.NameSpaces;
 import com.typesafe.config.ConfigFactory;
 
 public class UploadOntologyTest extends StepTest{
 	private static UploadOntologyTest test = new UploadOntologyTest();
+	private int CHEARTRIPLE = 5339;
+	private int COGATTRIPLE = 22620;
+	private int DOIDTRIPLE = 222793;
+	private int HASCOTRIPLE = 511;
+	private int OWLTRIPLE = 450;
+	private int PATOTRIPLE = 33947;
+	private int PROVTRIPLE = 1617;
+	private int RDFTRIPLE = 102;
+	private int RDFSTRIPLE = 87;
+	private int SIOTRIPLE = 12184;
+	private int UOTRIPLE = 5674;
+	private int VSTOITRIPLE = 497;
 	private int TOTALTRIPLE = 305821;
 	
 	private UploadOntologyTest() {}
@@ -85,6 +98,63 @@ public class UploadOntologyTest extends StepTest{
         metadata.loadOntologies(0, "confirmedCache");
         
         //check result after upload ontology from cache
+        for(NameSpace ns : NameSpaces.getInstance().getOrderedNamespacesAsList())
+        {
+        	String ab = ns.getAbbreviation();
+        	int num = ns.getNumberOfLoadedTriples();
+        	switch (ab) {
+			case "chear":
+				assertTrue(String.format("Triples after loading \"chear\" ontologies should be %d, but was %d", CHEARTRIPLE, num), num == CHEARTRIPLE);
+				break;
+				
+			case "cogat":
+				assertTrue(String.format("Triples after loading \"cogat\" ontologies should be %d, but was %d", COGATTRIPLE, num), num == COGATTRIPLE);
+				break;
+				
+			case "doid":
+				assertTrue(String.format("Triples after loading \"doid\" ontologies should be %d, but was %d", DOIDTRIPLE, num), num == DOIDTRIPLE);
+				break;
+				
+			case "hasco":
+				assertTrue(String.format("Triples after loading \"hasco\" ontologies should be %d, but was %d", HASCOTRIPLE, num), num == HASCOTRIPLE);
+				break;
+				
+			case "owl":
+				assertTrue(String.format("Triples after loading \"owl\" ontologies should be %d, but was %d", OWLTRIPLE, num), num == OWLTRIPLE);
+				break;
+				
+			case "pato":
+				assertTrue(String.format("Triples after loading \"pato\" ontologies should be %d, but was %d", PATOTRIPLE, num), num == PATOTRIPLE);
+				break;
+				
+			case "prov":
+				assertTrue(String.format("Triples after loading \"prov\" ontologies should be %d, but was %d", PROVTRIPLE, num), num == PROVTRIPLE);
+				break;
+				
+			case "rdf":
+				assertTrue(String.format("Triples after loading \"rdf\" ontologies should be %d, but was %d", RDFTRIPLE, num), num == RDFTRIPLE);
+				break;
+				
+			case "rdfs":
+				assertTrue(String.format("Triples after loading \"rdfs\" ontologies should be %d, but was %d", RDFSTRIPLE, num), num == RDFSTRIPLE);
+				break;
+				
+			case "sio":
+				assertTrue(String.format("Triples after loading \"sio\" ontologies should be %d, but was %d", SIOTRIPLE, num), num == SIOTRIPLE);
+				break;
+				
+			case "uo":
+				assertTrue(String.format("Triples after loading \"uo\" ontologies should be %d, but was %d", UOTRIPLE, num), num == UOTRIPLE);
+				break;
+				
+			case "vstoi":
+				assertTrue(String.format("Triples after loading \"vstoi\" ontologies should be %d, but was %d", VSTOITRIPLE, num), num == VSTOITRIPLE);
+				break;
+			default:
+				break;
+			}
+        }
+        
         long tripleNum = metadata.totalTriples();
         assertTrue(String.format("Triples after loading all ontologies should be %d, but was %d", TOTALTRIPLE, tripleNum), tripleNum == TOTALTRIPLE);
 		System.out.println("[Step 3] Upload Ontology Test Pass. " + tripleNum);
