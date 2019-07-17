10f5b9d93039edb0042ef5ed06181b3751963d53
johnsm21 <johnsm21@rpi.edu>
2019-07-16 21:06:48 -0400

    Implemented next & prev functionality for ontology browser

--- public/javascripts/treeEntity.js
+++ public/javascripts/treeEntity.js
@@ -1,31 +1,31 @@
 
 !(function (d3) {
-    
+
     $("treecontent").empty();
-    
+
     var query_res = document.getElementById('entityquery');
     var results = query_res.dataset.results;
-    
+
     var margin = {top: 30, right: 20, bottom: 30, left: 20},
         width = 960 - margin.left - margin.right,
         barHeight = 20,
         barWidth = width * .8;
-    
+
     var i = 0,
         duration = 400,
         root;
-    
+
     var tree = d3.layout.tree()
         .nodeSize([0, 20]);
-    
+
     var diagonal = d3.svg.diagonal()
         .projection(function(d) { return [d.y, d.x]; });
-    
+
     var svg = d3.select("treecontent").append("svg")
         .attr("width", width + margin.left + margin.right)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
+
     function resetSelection(d){
         d._isSelected = false;
         var children = (d.children)?d.children:d._children;
@@ -33,7 +33,7 @@
         children.forEach(resetSelection);
         }
     }
-    
+
     function collapse(d) {
        if (d.children) {
           d._children = d.children;
@@ -41,7 +41,7 @@
           d.children = null;
        }
     }
-    
+
     function expand(d){
         var children = (d.children)?d.children:d._children;
         if (d._children) {
@@ -52,7 +52,7 @@
         children.forEach(expand);
         }
     }
-    
+
     function exactSearchTree(obj,search,path){
         if(obj.name === search){ //if search is found return, add the object to the path and return it
             obj._isSelected = true;  // mark is
@@ -76,7 +76,7 @@
         return false;
         }
     }
-    
+
     function searchTree(obj,search,path){
         if(obj.name.indexOf(search) != -1){ //if search is found return, add the object to the path and return it
             obj._isSelected = true;  // mark is
@@ -101,7 +101,32 @@
         return false;
         }
     }
-    
+
+    function getPartialMatches(obj, search){
+      var stack = [obj];
+      var result = [];
+
+      var searchString = search.toLowerCase()
+
+      while(stack.length > 0){
+         var i = stack.pop();
+
+         // Add node to the list if its a partial match
+         if(i.name.toLowerCase().indexOf(searchString) != -1){ //if search is found return, add the object to the path and return it
+            result.push(i.name);
+         }
+
+         // Add all child nodes to stack
+         if(i.children || i._children){ //if children are collapsed d3 object will have them instantiated as _children
+            var children = (i.children) ? i.children : i._children;
+            for(const child of children){
+               stack.push(child);
+            }
+         }
+      }
+      return result;
+   }
+
     function openPaths(paths){
         for(var i =0;i<paths.length;i++){
         if(paths[i].id !== "1"){//i.e. not root
@@ -114,7 +139,7 @@
         }
         }
     }
-    
+
     function initialize(d) {
         lastClikD = null;
         resetSelection(d);
@@ -126,39 +151,39 @@
         }
         }
     }
-    
+
     var flare = JSON.parse(results);
         flare.x0 = 0;
         flare.y0 = 0;
         root = flare;
         initialize(root);
         update(root);
-    
+
     function update(source) {
-    
+
         // Compute the flattened node list. TODO use d3.layout.hierarchy.
         var nodes = tree.nodes(root);
-    
+
         var height = Math.max(500, nodes.length * barHeight + margin.top + margin.bottom);
-    
+
         d3.select("svg").transition()
         .duration(duration)
         .attr("height", height);
-    
+
         // Compute the "layout".
         nodes.forEach(function(n, i) {
             n.x = i * barHeight;
         });
-    
+
         // Update the nodes…
         var node = svg.selectAll("g.node")
         .data(nodes, function(d) { return d.id || (d.id = ++i); });
-    
+
         var nodeEnter = node.enter().append("g")
         .attr("class", "node")
         .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
         .style("opacity", 1e-6);
-    
+
         // Enter any new nodes at the parent's previous position.
         nodeEnter.append("rect")
         .attr("y", -barHeight / 2)
@@ -166,11 +191,11 @@
         .attr("width", barWidth)
         .style("fill", color)
         .on("click", click);
-    
+
         nodeEnter.append("text")
         .attr("dy", 3.5)
         .attr("dx", 5.5);
-    
+
         node.select('text')
         .text(function(d) {
             if (d.children) {
@@ -181,31 +206,31 @@
                 return d.name;
             }
             });
-    
+
         // Transition nodes to their new position.
         nodeEnter.transition()
         .duration(duration)
         .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
         .style("opacity", 1);
-    
+
         node.transition()
         .duration(duration)
         .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
         .style("opacity", 1)
         .select("rect")
         .style("fill", color);
-    
+
         // Transition exiting nodes to the parent's new position.
         node.exit().transition()
         .duration(duration)
         .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
         .style("opacity", 1e-6)
         .remove();
-    
+
         // Update the links…
         var link = svg.selectAll("path.link")
         .data(tree.links(nodes), function(d) { return d.target.id; });
-    
+
         // Enter any new links at the parent's previous position.
         link.enter().insert("path", "g")
         .attr("class", "link")
@@ -216,12 +241,12 @@
         .transition()
         .duration(duration)
         .attr("d", diagonal);
-    
+
         // Transition links to their new position.
         link.transition()
         .duration(duration)
         .attr("d", diagonal);
-    
+
         // Transition exiting nodes to the parent's new position.
         link.exit().transition()
         .duration(duration)
@@ -230,14 +255,14 @@
             return diagonal({source: o, target: o});
             })
         .remove();
-    
+
         // Stash the old positions for transition.
         nodes.forEach(function(d) {
             d.x0 = d.x;
             d.y0 = d.y;
         });
     }
-    
+
     // Toggle children on click.
     var lastClickD = null;
     document.getElementById('copyvalue').disabled = true;
@@ -257,40 +282,90 @@
         document.getElementById('copyvalue').disabled = false;
         update(d);
     }
-    
+
     document.getElementById("collapse").onclick = function() {
         resetSelection(root);
         lastClickD = null;
         root.children.forEach(collapse);
         update(root);
     };
-    
+
     document.getElementById("expand").onclick = function() {
         expand(root);
         update(root);
     };
-    
+
     document.getElementById("reset").onclick = function() {
         initialize(root);
         update(root);
+        searchResults = null;
+        searchIndex = 0;
     };
-    
+
+
+    var searchResults = null;
+    var searchIndex = 0;
+
     document.getElementById("findTerm").onclick = function() {
-        root.children.forEach(collapse);
-        var paths = searchTree(root,document.getElementById("searchValue").value,[]);
-        if(typeof(paths) !== "undefined"){
-        openPaths(paths);
-        }
-        else{
-        alert(document.getElementById("searchValue").value + " not found!");
-        }
-    };
-    
+      // Reset View
+      resetSelection(root);
+      lastClickD = null;
+      root.children.forEach(collapse);
+      update(root);
+
+      searchResults = getPartialMatches(root, document.getElementById("searchValue").value);
+      searchIndex = 0;
+
+      if(searchResults.length > 0){
+         var paths = exactSearchTree(root, searchResults[searchIndex], []);
+         openPaths(paths);
+      }
+      else{
+         searchResults = null;
+         searchIndex = 0;
+         alert(document.getElementById("searchValue").value + " not found!");
+      }
+   };
+
+   document.getElementById("findNext").onclick = function() {
+      if(searchResults){
+         if((searchIndex + 1) < searchResults.length){
+            // Reset View
+            resetSelection(root);
+            lastClickD = null;
+            root.children.forEach(collapse);
+            update(root);
+
+            // New Selection
+            searchIndex = searchIndex + 1;
+            var paths = exactSearchTree(root, searchResults[searchIndex], []);
+            openPaths(paths);
+         }
+      }
+   };
+
+   document.getElementById("findPrev").onclick = function() {
+      if(searchResults){
+         if((searchIndex - 1) >= 0){
+            // Reset View
+            resetSelection(root);
+            lastClickD = null;
+            root.children.forEach(collapse);
+            update(root);
+
+            // New Selection
+            searchIndex = searchIndex - 1;
+            var paths = exactSearchTree(root, searchResults[searchIndex], []);
+            openPaths(paths);
+         }
+      }
+   };
+
     document.getElementById("copyvalue").onclick = function() {
        $('#newEntity').val(lastClickD.name);
     }
-    
-    
+
+
     var cm=document.querySelector(".custom-cm");
     function showContextMenu(show=true){
         cm.style.display=show ?'block' :"none";
@@ -315,7 +390,7 @@
     var posy = 0;
 
     if (!e) var e = window.event;
-    
+
     if (e.pageX || e.pageY) {
       posx = e.clientX;
       posy = e.clientY;
@@ -332,41 +407,41 @@
     function positionMenu(e) {
         clickCoords = getPosition(e);
         clickCoordsX = clickCoords.x;
-      
+
         clickCoordsY = clickCoords.y;
-       
-    
+
+
         menuWidth = cm.offsetWidth + 4;
         menuHeight = cm.offsetHeight + 4;
-    
+
         windowWidth = window.innerWidth;
         windowHeight = window.innerHeight;
-    
+
         if ( (windowWidth - clickCoordsX) < menuWidth ) {
             cm.style.left = windowWidth - menuWidth + "px";
         } else {
             cm.style.left = clickCoordsX + "px";
         }
-    
+
         if ( (windowHeight - clickCoordsY) < menuHeight ) {
             cm.style.top = windowHeight - menuHeight + "px";
         } else {
             cm.style.top = clickCoordsY + "px";
-        
+
         }
 
       }
-    
+
     var onto;
     // Capture Right Click event
     if (document.addEventListener) { // IE >= 9; other browsers
        document.addEventListener('contextmenu', function(e) {
-       
+
           onto=(e.target.__data__.name.split("[")[0])+(e.target.__data__.name.split("[")[1].split("]")[0]);
           e.preventDefault();
           showContextMenu();
           positionMenu(e);
-        
+
        }, false);
        document.addEventListener("click",function(e){
         showContextMenu(false);
@@ -381,56 +456,56 @@
        });
     }
 
-   
+
     var ct=-1;
     updateCartCounter();
     //createMenu(ct);
-    
+
 
     var additem=document.getElementById("thisitem");
-    additem.addEventListener('click', function(e) { 
+    additem.addEventListener('click', function(e) {
         var str= onto;
          $.ajax({
             type : 'GET',
-            
+
             url : 'http://localhost:9000/hadatac/annotator/sddeditor_v2/addToCart',
             data : {
                 ontology: str
             },
             success : function(data) {
-                updateCartCounter(); 
+                updateCartCounter();
                 ct++;
                 createMenu(ct);
             }
         });
-        
-        
-                
+
+
+
     }, false);
-        
-     
-    
-      
+
+
+
+
     function updateCartCounter(){
         //document.getElementById("cartctr").innerHTML=t.length;
         var cart_ctr=0;
         $.ajax({
             type : 'GET',
-            
+
             url : 'http://localhost:9000/hadatac/annotator/sddeditor_v2/getCart',
             data : {
                 //cartamount: cart_ctr
             },
-            success : function(data) {                               
+            success : function(data) {
                 document.getElementById("cartctr").innerHTML=data.length;
             }
         });
-        
+
      }
      function removeCartItem(itemToBeRemoved){
         $.ajax({
             type : 'GET',
-            
+
             url : 'http://localhost:9000/hadatac/annotator/sddeditor_v2/removeFromCart',
             data : {
                 item: itemToBeRemoved
@@ -438,18 +513,18 @@
             success : function(data) {
                 console.log(data);
                 updateCartCounter();
-                               
-                
+
+
             }
         });
-        
+
      }
-     
+
 
     function createMenu(ct){
         $.ajax({
             type : 'GET',
-            
+
             url : 'http://localhost:9000/hadatac/annotator/sddeditor_v2/getCart',
             data : {
                 //cartamount: cart_ctr
@@ -463,7 +538,7 @@
                     span.innerHTML = '&times;';
                     var li = document.createElement("li");
                     span.setAttribute("class", "remove");
-                    
+
                     li.appendChild(document.createTextNode(data[i]+" "));
                     li.appendChild(span);
                     select.appendChild(li);
@@ -472,19 +547,16 @@
                     //updateCartCounter()
                 };
                 }
-                               
-                
+
+
             }
         });
     }
-   
-    
+
+
     function color(d) {
         if (d._isSelected) return 'red';
         return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
     }
-    
-    })(d3);
 
-     
-    
+    })(d3);
