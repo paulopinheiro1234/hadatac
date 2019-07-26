225d1ed13509289ea6180b256c0221da0cd81488
Paulo Pinheiro <pp3223@gmail.com>
2019-07-26 07:38:04 -0400

    added support for ingesting dd headers into sdds

--- app/org/hadatac/console/controllers/workingfiles/FileHeadersIntoSDD.java
+++ app/org/hadatac/console/controllers/workingfiles/FileHeadersIntoSDD.java
@@ -19,14 +19,14 @@ import play.mvc.Result;
 public class FileHeadersIntoSDD extends Controller {
 
     @Restrict(@Group(AuthApplication.DATA_OWNER_ROLE))
-	public Result createHeaders(String dir, String sdd_id) {
+	public Result createHeaders(String dir, String dd_id, String headerSheetColumn, String commentSheetColumn) {
         final SysUser user = AuthApplication.getLocalUser(session());
 
     	DataFile dataFile = null;
         if (user.isDataManager()) {
-            dataFile = DataFile.findById(sdd_id);
+            dataFile = DataFile.findById(dd_id);
         } else {
-            dataFile = DataFile.findByIdAndEmail(sdd_id, user.getEmail());
+            dataFile = DataFile.findByIdAndEmail(dd_id, user.getEmail());
         }
         
         if (null == dataFile) {
@@ -36,17 +36,20 @@ public class FileHeadersIntoSDD extends Controller {
         DataFile dirFile = new DataFile("/");
         dirFile.setStatus(DataFile.WORKING);
         
-		return ok(fileHeadersIntoSDD.render(dir, dataFile.getFileName(), dirFile));
+		return ok(fileHeadersIntoSDD.render(dir, dataFile.getFileName(), dirFile, headerSheetColumn, commentSheetColumn));
 	}
 
     @Restrict(@Group(AuthApplication.DATA_OWNER_ROLE))
-    public Result postCreateHeaders(String dir, String sdd_uri) {
-        return createHeaders(dir, sdd_uri);
+    public Result postCreateHeaders(String dir, String dd_uri, String headerSheetColumn, String commentSheetColumn) {
+        return createHeaders(dir, dd_uri, headerSheetColumn, commentSheetColumn);
     }
 
     @Restrict(@Group(AuthApplication.DATA_OWNER_ROLE))
-    public Result createHeadersForm(String dir, String sdd_uri) {
-        return redirect(routes.WorkingFiles.index(dir, "."));
+    public Result createHeadersForm(String dir, String dd_uri, String sdd_uri, String headerSheetColumn, String commentSheetColumn) {
+
+    	/* ADD THE DD CONTENT FROM DD_URI INTO THE SDD)URI  */
+    	
+    	return redirect(routes.WorkingFiles.index(dir, "."));
     }
 
 }
