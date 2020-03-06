07da1a62d008e3bb4a442fa6fac4fa36431b0e4e
Harry Wong <harryfwong@gmail.com>
2020-03-05 23:26:58 -0500

    ran dos2unix on all files to remove windows new lines added blank line so that we could commit

--- create_namespaces.sh
+++ create_namespaces.sh
@@ -9,3 +9,4 @@ echo ""
 echo "=== Creating store_users namespace..."
 curl -X POST --data-binary @blazegraph/store_users.properties -H 'Content-Type:text/plain' http://localhost:8080/blazegraph/namespace
 echo ""
+
